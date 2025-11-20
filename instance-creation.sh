830a6142e67f675871ab7eb2bfaeacaafea3e2f8
vkg1209 <vinayakumargantala@gmail.com>
2025-11-20 18:33:54 +0530

    dfdf

--- instance-creation.sh
+++ instance-creation.sh
@@ -12,34 +12,25 @@ DOMAIN_NAME="bloombear.fun"
 LOG_FOLDER="/var/log/shell-scripting"
 LOG_FILE="$LOG_FOLDER/$0.log"
 
-# validating if the command is success or not
-VALIDATE() {
-    if [ $1 -eq 0 ]; then
-        echo -e "$2 ... $G SUCCESS $N"
-    else 
-        echo -e "$2 ... $R FAILED $N"
-        exit 1
-    fi
-}
+set -euo pipefail
 
-# creating an instances and storing the instance ids
+# Creating instances from the arguments passed to the script.
 for instance in $@
 do
+    # Creating the EC2 instance and recording its instance ID in the INSTANCE_ID variable
     INSTANCE_ID=$(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${instance}}]" --query 'Instances[0].InstanceId' --output text) &>> LOG_FILE
     VALIDATE $? "Creating $instance  Instance"
 
+    # Logic that retrieves the public IP for frontend instances and the private IP for backend instances.
     if [ $instance == "frontend" ]; then
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text) &>> LOG_FILE
-        VALIDATE $? "Getting Public IP Address"
-        echo -e "$G Public IP: $N $IP"
         RECORD_NAME="$DOMAIN_NAME"
     else 
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text) &>> LOG_FILE
-        VALIDATE $? "Getting Private IP Address"
-        echo -e "$G Private IP: $N $IP"
         RECORD_NAME="$instance.$DOMAIN_NAME"
     fi
 
+    # Updating the Route53 DNS records.
     aws route53 change-resource-record-sets \
   --hosted-zone-id Z01953923TROREUOJRMSG \
   --change-batch '
@@ -58,8 +49,6 @@ do
     }]
   }
   '
-    VALIDATE $? "Creating or Updating the DNS Record"
-    echo "-------------------------"
 
 done
 
